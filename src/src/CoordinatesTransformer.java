92e7f208391c1bd84284eaf6120360e0b936d229
Exodia7 <valentin.ringlet@gmail.com>
2019-06-21 11:06:18 +0200

    modified internal representation from two doubles to a Vector2D object, also made it flip the y-positions such that the solar system actually look correct to represent the data

--- src/src/CoordinatesTransformer.java
+++ src/src/CoordinatesTransformer.java
@@ -22,36 +22,22 @@ public class CoordinatesTransformer {
 		posModifier = new Vector2D(newModifiedPos);
 	}
 
-	/*
-	public double getModifiedX () {
-		return xModifier;
-	}
-
-	public void setModifiedX (double modifiedX) {
-		xModifier = modifiedX;
-	}
-
-	public double getModifiedY () {
-		return yModifier;
-	}
-
-	public void setModifiedY (double modifiedY) {
-		yModifier = modifiedY;
-	}
-	*/
-
 	public Vector2D modelToOtherPosition (Vector2D pos) {
-		return new Vector2D(posModifier).add(getScaledPos(pos));
-		//return new Vector2D(xModifier + getScaledSize(pos.x), yModifier + getScaledSize(pos.y));
+		//First, retrieve the position modifier
+		Vector2D otherModelPos = new Vector2D(posModifier);
+
+		//And add the actual position scaled to the corresponding value,
+		Vector2D scaledPos = new Vector2D(getScaledPos(pos));
+		//which we flip the y-position of, since the negative y goes up and a positive y down
+		scaledPos.setY(- scaledPos.getY());
+
+		//Sum the two, and return the result
+		otherModelPos.add(scaledPos);
+
+		return otherModelPos;
 	}
 
 	public Vector2D getScaledPos (Vector2D unscaledPos) {
 		return new Vector2D(unscaledPos).divide(scale);
 	}
-
-	/*
-	public double getScaledSize (double distance) {
-		return distance / scale;
-	}
-	*/
 }
